f39e7ef174b97bf4a67bac9574107e7ac2c40d65
rasyidrw <rasyidrespati.30@gmail.com>
2020-03-04 16:12:46 +0700

    sec commit

--- Calculator SwiftUI/ContentView.swift
+++ Calculator SwiftUI/ContentView.swift
@@ -8,13 +8,73 @@
 
 import SwiftUI
 
+enum CalculatorButton : String {
+    
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero:
+            return "0"
+        case .one:
+            return "1"
+        case .two:
+            return "2"
+        case .three:
+            return "3"
+            case .plus:
+            return "+"
+        case .four:
+            return "4"
+        case .five:
+            return "5"
+        case .six:
+            return "6"
+            case .minus:
+            return "-"
+        case .seven:
+            return "7"
+        case .eight:
+            return "8"
+        case .nine:
+            return "9"
+            case .multiply:
+            return "X"
+            case .plusMinus:
+            return "+/-"
+            case .percent:
+            return "%"
+            case .divide:
+            return "/"
+        default:
+            return "AC"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(.orange)
+        }
+    }
+    
+}
+
 struct ContentView: View {
     
-    let buttons = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", ".", "="]
+    let buttons : [[CalculatorButton]] = [
+        
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        
     ]
     
     var body: some View {
@@ -32,11 +92,11 @@ struct ContentView: View {
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12) {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
+                            Text(button.title)
                                 .font(.system(size:32))
                                 .frame(width: self.buttonWidth(), height: self.buttonWidth())
                                 .foregroundColor(.white)
-                                .background(Color.yellow)
+                                .background(button.backgroundColor)
                                 .cornerRadius(self.buttonWidth())
                         }
                     }
