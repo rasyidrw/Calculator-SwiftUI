ada19224cad392fd53ede86f608155c7728081ee
rasyidrw <rasyidrespati.30@gmail.com>
2020-03-04 13:42:40 +0700

    Initial Commit

---
+++ Calculator SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator SwiftUI
+//
+//  Created by Rasyid Respati Wiriaatmaja on 04/03/20.
+//  Copyright © 2020 rasyidrw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
